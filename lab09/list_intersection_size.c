de1b1b1c236a2d0d6ca6defd84bb94cb11c8dfa3
Soorria Saruva <soorria.ss@gmail.com>
2019-11-15 11:20:17 +1100

    complete lab09

---
+++ lab09/list_intersection_size.c
@@ -0,0 +1,76 @@
+//
+// COMP1511 lab09 - Determine how many elements are the same in two lists
+//
+// By Soorriamoorthy Saruva (z5258068)
+//
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+    struct node *next;
+    int data;
+};
+
+int intersection_size(struct node *head1, struct node *head2);
+struct node *strings_to_list(int len, char *strings[]);
+int includes(int num, struct node *head);
+
+// DO NOT CHANGE THIS MAIN FUNCTION
+int main(int argc, char *argv[]) {
+    // create two linked lists from command line arguments
+    int dash_arg = argc - 1;
+    while (dash_arg > 0 && strcmp(argv[dash_arg], "-") != 0) {
+        dash_arg = dash_arg - 1;
+    }
+    struct node *head1 = strings_to_list(dash_arg - 1, &argv[1]);
+    struct node *head2 = strings_to_list(argc - dash_arg - 1, &argv[dash_arg + 1]);
+
+    int result = intersection_size(head1, head2);
+    printf("%d\n", result);
+
+    return 0;
+}
+
+// return the number of values which occur in both linked lists
+// no value is repeated in either list
+int intersection_size(struct node *head1, struct node *head2) {
+    int counter = 0;
+    struct node *current = head1;
+
+    while (current != NULL) {
+        if (includes(current->data, head2)) {
+            counter++;
+        }
+        current = current->next;
+    }
+
+    return counter;
+}
+
+int includes(int num, struct node *head) {
+    while (head != NULL) {
+        if (head->data == num) {
+            return 1;
+        }
+        head = head->next;
+    }
+
+    return 0;
+}
+
+// DO NOT CHANGE THIS FUNCTION
+// create linked list from array of strings
+struct node *strings_to_list(int len, char *strings[]) {
+    struct node *head = NULL;
+    for (int i = len - 1; i >= 0; i = i - 1) {
+        struct node *n = malloc(sizeof(struct node));
+        assert(n != NULL);
+        n->next = head;
+        n->data = atoi(strings[i]);
+        head = n;
+    }
+    return head;
+}
